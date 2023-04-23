b7533dbccaeeb2f143cc4623215589e6fcb79d8a
Dennis <skyggendk@gmail.com>
2023-04-23 22:48:53 +0200

    Tilføjet test fil for at undersøge overførelse til Wiki i DevOps.

---
+++ Domain/Models/Konsulent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Konsulent
+    {
+        /// <summary>
+        /// Dette er en test metode tilføjet for at teste, hvordan kode bliver tilføjet til Wiki inde i Azure Devops.
+        /// </summary>
+        /// <param name="number">Tager en integer som parameter</param>
+        /// <returns>Returnerer den givne integer som en string</returns>
+        public string TestMethodForDocumentation(int number)
+        {
+            return number.ToString();
+        }
+    }
+}
