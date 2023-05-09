7d4944cd3cc6c1c892ac2c29cab8ee2b7464f005
hammersky30@gmail.com <hammersky30@gmail.com>
2023-05-09 22:12:20 +0200

    Tilføje lidt til manager.cs og oprettet ny form: ViewProjects.cs [Design] + ViewProjects.cs

---
+++ GUI/ViewProjects.cs
@@ -0,0 +1,41 @@
+﻿using DAL.Linq;
+using Domain.Services;
+using Interfaces.Models;
+using Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class ViewProjects : Form
+    {
+        DataClassesDataContext dbContext = new DataClassesDataContext(DbConnectionString.ConnectionString);
+        public int userId { get; set; }
+
+        public ViewProjects(int userId)
+        {
+            InitializeComponent();
+            this.userId = userId;
+            ProjectsForManager();
+        }
+
+        private void ProjectsForManager()
+        {
+            var projects = dbContext.Projects.Where(p => p.User_ID == userId);
+            dgv_Viewproject.DataSource = projects.ToList();
+
+        }
+
+        private void dgv_Viewproject_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ProjectsForManager();
+        }
+    }
+}
