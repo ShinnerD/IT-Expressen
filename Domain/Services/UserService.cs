e70c6d4a5f829586ffe5a34937e60d37a09819eb
Dennis <skyggendk@gmail.com>
2023-05-09 15:00:08 +0200

    UserService update with added methods.

--- Domain/Services/UserService.cs
+++ Domain/Services/UserService.cs
@@ -1,19 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Repository;
 using Interfaces.Models;
-
 
 namespace Domain.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
+        private IUserRepository userRepo = new UserRepository();
+
         public List<IUserModel> GetAllUsers()
         {
-            var  userRepo = new DAL.Repository.UserRepository();
-            return userRepo.GetAllUsers();    
+            return userRepo.GetAllUsers();
+        }
+
+        public IUserModel GetUser(string userName)
+        {
+            return userRepo.GetUser(userName);
+        }
+
+        public bool UsernameExists(string userName)
+        {
+            return GetAllUsers().Exists(i => i.UserName == userName);
+        }
+
+        public string GetUserType(string userName)
+        {
+            if (UsernameExists(userName))
+            {
+                return userRepo.GetUser(userName).UserType;
+            }
+            return "";
         }
     }
 }
