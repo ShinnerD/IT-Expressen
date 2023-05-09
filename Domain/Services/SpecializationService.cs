9ba519286bdf410a2a1f22f992ebf42cba62816b
Dennis <skyggendk@gmail.com>
2023-05-09 14:57:26 +0200

    Added SpecialisationService Class to Domain Services

---
+++ Domain/Services/SpecializationService.cs
@@ -0,0 +1,20 @@
+﻿using Interfaces.Services;
+
+namespace Domain.Services
+{
+    /// <summary>
+    /// Service class that has methods for retrieving and manipulating skills/specializations. /DK
+    /// </summary>
+    public class SpecializationService : ISpecializationService
+    {
+        /// <returns>Returns a list of strings that represent the current specializations in the Database</returns>
+        public List<string> ListDefinedSpecializations()
+        {
+            List<string> result;
+            var repo = new DAL.Repository.SpecializationRepository();
+
+            result = repo.GetCurrentSpecializationsList();
+            return result;
+        }
+    }
+}
