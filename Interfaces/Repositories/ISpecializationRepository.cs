42716bbbaa4675f104d905675327b1600bed56d7
Dennis <skyggendk@gmail.com>
2023-05-09 14:45:50 +0200

    Added ISpecializationRepository

---
+++ Interfaces/Repositories/ISpecializationRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace Interfaces.Repositories
+{
+    /// <summary>
+    /// Interface for accessing methods from the SpecializationRepository class.
+    /// Contains methods for retrieving specialization-specific data or adding specializations to a project in the database.
+    /// </summary>
+    public interface ISpecializationRepository
+    {
+        /// <returns>A list of string representing specializations defined in the database.</returns>
+        List<string> GetCurrentSpecializationsList();
+
+        /// <returns>An int representing the generated identifier ID for a specialization in the database.</returns>
+        int GetSpecializationID(string specialization);
+
+        void AddSpecializationsToProject(int projectId, List<string> specializations);
+    }
+}
