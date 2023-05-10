d9ea3ac1ad6e4f9f441398c1efa08fda607df890
Dennis <skyggendk@gmail.com>
2023-05-10 13:57:44 +0200

    Correction in

--- GUI/ViewProjects.cs
+++ GUI/ViewProjects.cs
@@ -1,37 +1,23 @@
-﻿using DAL.Linq;
-using Domain.Services;
-using Interfaces.Models;
-using Interfaces.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Interfaces.Services;
 
 namespace GUI
 {
     public partial class ViewProjects : Form
     {
-        IProjectService projectService = new Domain.Services.ProjectService();
+        private IProjectService projectService = new Domain.Services.ProjectService();
 
-        public int userId { get; set; }
+        public int UserId { get; set; }
 
         public ViewProjects(int userId)
         {
             InitializeComponent();
-            this.userId = userId;
+            UserId = userId;
             ProjectsForManager();
         }
 
         private void ProjectsForManager()
         {
-            var projects = dbContext.Projects.Where(p => p.User_ID == userId);
-            dgv_Viewproject.DataSource = projects.ToList();
-
+            dgv_Viewproject.DataSource = projectService.GetUserProjects(UserId);
         }
 
         private void dgv_Viewproject_CellContentClick(object sender, DataGridViewCellEventArgs e)
