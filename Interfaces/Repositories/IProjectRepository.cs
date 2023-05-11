f4ccce61f2bb22b368e1aeddc9a00562ce797617
Dennis <skyggendk@gmail.com>
2023-05-11 13:41:05 +0200

    Added Comments and documentation for ProjectService, ProjectRepository, SpecializationService, SpecializationRepository and all their respective Interfaces.

--- Interfaces/Repositories/IProjectRepository.cs
+++ Interfaces/Repositories/IProjectRepository.cs
@@ -9,14 +9,23 @@ namespace Interfaces.Repositories
     public interface IProjectRepository
     {
         /// <summary>
-        /// Adds a project with all the details required in the parameter of the method to the database.
+        /// Adds a project with all the details required in the parameter of the method to the database. /DK
         /// </summary>
         void Add(string userName, string title, string description, DateTime startDate, DateTime endDate, List<string> specializations);
 
-
+        /// <summary>
+        /// Returns a List of IProjectModels with projects related to the User Id provided. /DK
+        /// </summary>
         List<IProjectModel> GetUserProjects(int userId);
 
+        /// <summary>
+        /// Returns an IProjectModel for the project matching the provided Project Id. /DK
+        /// </summary>
         IProjectModel GetProject(int projectId);
+
+        /// <summary>
+        /// Updates an existing project in the database to match the IProjectModel provided in the parameters. /DK
+        /// </summary>
         void UpdateProject(IProjectModel project);
     }
 }
