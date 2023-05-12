79d558e0e1232aab151718714164d279dd7b20fe
Dennis <skyggendk@gmail.com>
2023-05-12 11:38:49 +0200

    Deleted Duplicate Method from IProjectService

--- Interfaces/Repositories/IProjectRepository.cs
+++ Interfaces/Repositories/IProjectRepository.cs
@@ -42,11 +42,7 @@ namespace Interfaces.Repositories
         /// Retrieves a List of IProjectModels in which each project requires at least one of the specializations specified. /DK
         /// </summary>
         List<IProjectModel> GetProjectsFromAnySpecializations(List<string> specializations);
-        void UpdateProject(IProjectModel project);
 
         List<IProjectModel> SearchProjects(string searchTerm, int userId);
-
-        
-        
     }
 }
