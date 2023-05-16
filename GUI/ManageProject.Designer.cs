21e736dd53d6ff1b35c2d349af2936b718e3d872
Martin Saxtorff <martinsaxtorff@hotmail.com>
2023-05-17 00:10:15 +0200

    temp

---
+++ GUI/ManageProject.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace GUI
+{
+    partial class ManageProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grpBoxProfileInfo = new GroupBox();
+            bt_EditProfileCancel = new Button();
+            lblUserCreationDate = new Label();
+            bt_EditProfile = new Button();
+            label2 = new Label();
+            tb_ProjectID = new TextBox();
+            label7 = new Label();
+            tb_Country = new TextBox();
+            tb_ProjectTitle = new TextBox();
+            label9 = new Label();
+            tb_ProjectModifiedDate = new TextBox();
+            label8 = new Label();
+            label3 = new Label();
+            tb_ProjectEndDate = new TextBox();
+            tb_ProjectOwner = new TextBox();
+            tb_ProjectStartDate = new TextBox();
+            tb_ProjectStatus = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label6 = new Label();
+            grpBoxProfileInfo.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpBoxProfileInfo
+            // 
+            grpBoxProfileInfo.Controls.Add(bt_EditProfileCancel);
+            grpBoxProfileInfo.Controls.Add(lblUserCreationDate);
+            grpBoxProfileInfo.Controls.Add(bt_EditProfile);
+            grpBoxProfileInfo.Controls.Add(label2);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectID);
+            grpBoxProfileInfo.Controls.Add(label7);
+            grpBoxProfileInfo.Controls.Add(tb_Country);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectTitle);
+            grpBoxProfileInfo.Controls.Add(label9);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectModifiedDate);
+            grpBoxProfileInfo.Controls.Add(label8);
+            grpBoxProfileInfo.Controls.Add(label3);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectEndDate);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectOwner);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectStartDate);
+            grpBoxProfileInfo.Controls.Add(tb_ProjectStatus);
+            grpBoxProfileInfo.Controls.Add(label5);
+            grpBoxProfileInfo.Controls.Add(label4);
+            grpBoxProfileInfo.Controls.Add(label6);
+            grpBoxProfileInfo.Dock = DockStyle.Top;
+            grpBoxProfileInfo.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            grpBoxProfileInfo.Location = new Point(0, 0);
+            grpBoxProfileInfo.Name = "grpBoxProfileInfo";
+            grpBoxProfileInfo.Size = new Size(699, 157);
+            grpBoxProfileInfo.TabIndex = 23;
+            grpBoxProfileInfo.TabStop = false;
+            grpBoxProfileInfo.Text = "User Name";
+            // 
+            // bt_EditProfileCancel
+            // 
+            bt_EditProfileCancel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            bt_EditProfileCancel.Enabled = false;
+            bt_EditProfileCancel.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            bt_EditProfileCancel.Location = new Point(983, 126);
+            bt_EditProfileCancel.Margin = new Padding(2);
+            bt_EditProfileCancel.Name = "bt_EditProfileCancel";
+            bt_EditProfileCancel.Size = new Size(100, 25);
+            bt_EditProfileCancel.TabIndex = 9;
+            bt_EditProfileCancel.Text = "Cancel";
+            bt_EditProfileCancel.UseVisualStyleBackColor = true;
+            bt_EditProfileCancel.Visible = false;
+            // 
+            // lblUserCreationDate
+            // 
+            lblUserCreationDate.AutoSize = true;
+            lblUserCreationDate.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic, GraphicsUnit.Point);
+            lblUserCreationDate.Location = new Point(219, 129);
+            lblUserCreationDate.Name = "lblUserCreationDate";
+            lblUserCreationDate.Size = new Size(186, 17);
+            lblUserCreationDate.TabIndex = 100;
+            lblUserCreationDate.Text = "You've been a user for 154 days";
+            // 
+            // bt_EditProfile
+            // 
+            bt_EditProfile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            bt_EditProfile.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            bt_EditProfile.Location = new Point(1087, 126);
+            bt_EditProfile.Margin = new Padding(2);
+            bt_EditProfile.Name = "bt_EditProfile";
+            bt_EditProfile.Size = new Size(100, 25);
+            bt_EditProfile.TabIndex = 9;
+            bt_EditProfile.Text = "Edit Profile";
+            bt_EditProfile.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(26, 25);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Project ID";
+            // 
+            // tb_ProjectID
+            // 
+            tb_ProjectID.BackColor = SystemColors.ControlLight;
+            tb_ProjectID.BorderStyle = BorderStyle.None;
+            tb_ProjectID.Enabled = false;
+            tb_ProjectID.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectID.Location = new Point(88, 22);
+            tb_ProjectID.Margin = new Padding(2);
+            tb_ProjectID.Name = "tb_ProjectID";
+            tb_ProjectID.Size = new Size(208, 16);
+            tb_ProjectID.TabIndex = 1;
+            tb_ProjectID.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(52, 51);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(32, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Title:";
+            // 
+            // tb_Country
+            // 
+            tb_Country.BackColor = SystemColors.ControlLight;
+            tb_Country.BorderStyle = BorderStyle.None;
+            tb_Country.Enabled = false;
+            tb_Country.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_Country.Location = new Point(431, 101);
+            tb_Country.Margin = new Padding(2);
+            tb_Country.Name = "tb_Country";
+            tb_Country.Size = new Size(263, 16);
+            tb_Country.TabIndex = 8;
+            tb_Country.TabStop = false;
+            // 
+            // tb_ProjectTitle
+            // 
+            tb_ProjectTitle.BackColor = SystemColors.ControlLight;
+            tb_ProjectTitle.BorderStyle = BorderStyle.None;
+            tb_ProjectTitle.Enabled = false;
+            tb_ProjectTitle.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectTitle.Location = new Point(88, 48);
+            tb_ProjectTitle.Margin = new Padding(2);
+            tb_ProjectTitle.Name = "tb_ProjectTitle";
+            tb_ProjectTitle.Size = new Size(208, 16);
+            tb_ProjectTitle.TabIndex = 2;
+            tb_ProjectTitle.TabStop = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(372, 101);
+            label9.Margin = new Padding(2, 0, 2, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(55, 15);
+            label9.TabIndex = 8;
+            label9.Text = "UNUSED!";
+            // 
+            // tb_ProjectModifiedDate
+            // 
+            tb_ProjectModifiedDate.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tb_ProjectModifiedDate.BackColor = SystemColors.ControlLight;
+            tb_ProjectModifiedDate.BorderStyle = BorderStyle.None;
+            tb_ProjectModifiedDate.Enabled = false;
+            tb_ProjectModifiedDate.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectModifiedDate.Location = new Point(431, 74);
+            tb_ProjectModifiedDate.Margin = new Padding(2);
+            tb_ProjectModifiedDate.Name = "tb_ProjectModifiedDate";
+            tb_ProjectModifiedDate.Size = new Size(263, 16);
+            tb_ProjectModifiedDate.TabIndex = 7;
+            tb_ProjectModifiedDate.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(305, 77);
+            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(122, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Project Last Modified:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(39, 77);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Owner:";
+            // 
+            // tb_ProjectEndDate
+            // 
+            tb_ProjectEndDate.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tb_ProjectEndDate.BackColor = SystemColors.ControlLight;
+            tb_ProjectEndDate.BorderStyle = BorderStyle.None;
+            tb_ProjectEndDate.Enabled = false;
+            tb_ProjectEndDate.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectEndDate.Location = new Point(431, 48);
+            tb_ProjectEndDate.Margin = new Padding(2);
+            tb_ProjectEndDate.Name = "tb_ProjectEndDate";
+            tb_ProjectEndDate.Size = new Size(263, 16);
+            tb_ProjectEndDate.TabIndex = 6;
+            tb_ProjectEndDate.TabStop = false;
+            // 
+            // tb_ProjectOwner
+            // 
+            tb_ProjectOwner.BackColor = SystemColors.ControlLight;
+            tb_ProjectOwner.BorderStyle = BorderStyle.None;
+            tb_ProjectOwner.Enabled = false;
+            tb_ProjectOwner.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectOwner.Location = new Point(88, 74);
+            tb_ProjectOwner.Margin = new Padding(2);
+            tb_ProjectOwner.Name = "tb_ProjectOwner";
+            tb_ProjectOwner.Size = new Size(208, 16);
+            tb_ProjectOwner.TabIndex = 3;
+            tb_ProjectOwner.TabStop = false;
+            // 
+            // tb_ProjectStartDate
+            // 
+            tb_ProjectStartDate.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tb_ProjectStartDate.BackColor = SystemColors.ControlLight;
+            tb_ProjectStartDate.BorderStyle = BorderStyle.None;
+            tb_ProjectStartDate.Enabled = false;
+            tb_ProjectStartDate.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectStartDate.Location = new Point(431, 22);
+            tb_ProjectStartDate.Margin = new Padding(2);
+            tb_ProjectStartDate.Name = "tb_ProjectStartDate";
+            tb_ProjectStartDate.Size = new Size(263, 16);
+            tb_ProjectStartDate.TabIndex = 5;
+            tb_ProjectStartDate.TabStop = false;
+            // 
+            // tb_ProjectStatus
+            // 
+            tb_ProjectStatus.BackColor = SystemColors.ControlLight;
+            tb_ProjectStatus.BorderStyle = BorderStyle.None;
+            tb_ProjectStatus.Enabled = false;
+            tb_ProjectStatus.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tb_ProjectStatus.Location = new Point(88, 101);
+            tb_ProjectStatus.Margin = new Padding(2);
+            tb_ProjectStatus.Name = "tb_ProjectStatus";
+            tb_ProjectStatus.Size = new Size(208, 16);
+            tb_ProjectStatus.TabIndex = 4;
+            tb_ProjectStatus.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(333, 51);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Project End Date";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(5, 101);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(82, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Project Status:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(326, 25);
+            label6.Margin = new Padding(2, 0, 2, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(101, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Project Start Date:";
+            // 
+            // ManageProject
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(699, 539);
+            Controls.Add(grpBoxProfileInfo);
+            Name = "ManageProject";
+            Text = "ManageProject";
+            grpBoxProfileInfo.ResumeLayout(false);
+            grpBoxProfileInfo.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grpBoxProfileInfo;
+        private Button bt_EditProfileCancel;
+        private Label lblUserCreationDate;
+        private Button bt_EditProfile;
+        private Label label2;
+        private TextBox tb_ProjectID;
+        private Label label7;
+        private TextBox tb_Country;
+        private TextBox tb_ProjectTitle;
+        private Label label9;
+        private TextBox tb_ProjectModifiedDate;
+        private Label label8;
+        private Label label3;
+        private TextBox tb_ProjectEndDate;
+        private TextBox tb_ProjectOwner;
+        private TextBox tb_ProjectStartDate;
+        private TextBox tb_ProjectStatus;
+        private Label label5;
+        private Label label4;
+        private Label label6;
+    }
+}
---
+++ GUI/ManageProject.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Models;
+using Domain.Services;
+using Interfaces.Models;
+using Interfaces.Services;
+
+namespace GUI
+{
+    public partial class ManageProject : Form
+    {
+        private int ProjectID { get; set; }
+        private IProjectModel ProjectGet { get; set; }
+
+        private IInvitesModel Invite { get; set; }
+        private IUserService UserServiceGet { get; set; }
+
+        public ManageProject(int projectId)
+        {
+            InitializeComponent();
+            ProjectID = projectId;
+            LoadProjectData();
+            GetProjectInfo();
+            LoadProjectData();
+        }
+        public ManageProject(IProjectModel projectModel)
+        {
+            InitializeComponent();
+            ProjectGet = projectModel;
+        }
+
+        private void GetProjectInfo()
+        {
+            IProjectService projectService = new ProjectService();
+            IInviteService inviteService = new InviteService();
+
+            ProjectGet = projectService.GetProject(ProjectID);
+            Invite = inviteService.GetInvitedProjectId(ProjectID);
+        }
+
+        private void LoadProjectData()
+        {
+
+            tb_ProjectID.Text = ProjectGet.ProjectId.ToString();
+            tb_ProjectTitle.Text = ProjectGet.Title.ToString();
+            tb_ProjectOwner.Text = ProjectGet.UserId.ToString();
+            tb_ProjectStartDate.Text = ProjectGet.ProjectStartDate.ToString();
+            tb_ProjectEndDate.Text = ProjectGet.ProjectEndDate.ToString();
+            tb_ProjectModifiedDate.Text = ProjectGet.ProjectModifyDate.ToString();
+        }
+
+    }
+}
