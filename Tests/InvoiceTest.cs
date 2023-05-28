fb7744e94cd5f16bfff1059a651b3e63696467b0
Erik Roest Holst <erho47738@edu.ucl.dk>
2023-05-28 18:30:41 +0200

    InvoiceUnitTest

---
+++ Tests/InvoiceTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Services;
+using DAL.Linq;
+using DAL.Models;
+using Interfaces.Models;
+using Interfaces.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Made by Erik Invoice UnitTest
+namespace Tests
+{
+    /// <summary>
+    /// Checking if there is a Invoice ID in the Database with a ID 1 if valid it return True.
+    /// </summary>
+    [TestClass]
+    public class InvoiceTest
+    {
+        [TestMethod]
+        public void CheckingInvoice_WithValidInvoiceId_ReturnsTrue()
+        {
+
+            var CheckingInvoice = new InvitationService();
+            var invoiceId = "1";
+
+
+            var result = CheckingInvoice.SendInvoice(invoiceId);
+
+
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// A metod too send a Invoice and checking if its Empty Null or have some valid.
+        /// </summary>
+        [TestClass]
+        public class InvitationService
+        {
+            [TestMethod]
+            public bool SendInvoice(string invoiceId)
+            {
+
+                if (string.IsNullOrEmpty(invoiceId))
+                {
+                    return false;
+                }
+
+
+
+                return true;
+
+            }
+        }
+    }
+}
--- Tests/IvniteTest - Copy.cs
+++ Tests/IvniteTest - Copy.cs
@@ -7,12 +7,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Made by Erik, Unit test of invite.
 namespace Tests
 {
     [TestClass]
     public class InviteTests
     {
+        /// <summary>
+        /// Checking if there is a Project ID in the Database with a ID 100 if valid it return True.
+        /// </summary>
         [TestMethod]
         public void SendInvitation_WithValidProjectId_ReturnsTrue()
         {
@@ -26,7 +29,9 @@ namespace Tests
            
             Assert.IsTrue(result);
         }
-
+        /// <summary>
+        /// Sending a Invitation too a Invalid Projekt ID That return False.
+        /// </summary>
         [TestMethod]
         public void SendInvitation_WithInvalidProjectId_ReturnsFalse()
         {
@@ -40,6 +45,9 @@ namespace Tests
             Assert.IsFalse(result);
         }
     }
+    /// <summary>
+    /// A metod too send a Invite and checking if its Empty Null or have some valid.
+    /// </summary>
     [TestClass]
     public class InvitationService
     {
