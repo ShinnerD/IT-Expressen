d9734c1dba1147a57c5b2c84515d9af4d99b5d19
Dennis Kempf <116871049+ShinnerD@users.noreply.github.com>
2023-06-16 18:08:07 +0200

    Update DbConnectionString.cs

--- DAL/Linq/DbConnectionString.cs
+++ DAL/Linq/DbConnectionString.cs
@@ -9,6 +9,6 @@ namespace DAL.Linq
     public static class DbConnectionString
     {
         public const string ConnectionString = 
-            "Data Source=ucl-jtm-sqlserver.database.windows.net;Initial Catalog=2-sem-gr-2;Persist Security Info=True;User ID=2-sem-gr-2-login;Password=Gr22Pa$$word!";
+            "*";
     }
 }
